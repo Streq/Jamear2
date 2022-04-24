ec8bcc13d5f292147dc3798f9ebcff998b728df7
streq <strequena@gmail.com>
2022-04-24 00:20:56 -0300

    fix rango

--- vision/vista.gd
+++ vision/vista.gd
@@ -17,7 +17,7 @@ func set_is_jugador(val:bool):
 func can_see(target: PhysicsBody2D):
 	if is_instance_valid(target):
 		var angle = rad2deg(get_angle_to(target.global_position))
-		if abs(angle) < angle_degs/2:
+		if abs(angle) < angle_degs/2 and get_parent().global_position.distance_squared_to(target.global_position) < radius*radius:
 			var space_state = get_world_2d().direct_space_state
 			var result = space_state.intersect_ray(get_parent().global_position, target.global_position, [get_parent()])
 			return (result.has("collider") and result.collider == target)
