4e99391ffe7f3f1a23d1f91a39d5c2954a981bf1
streq <strequena@gmail.com>
2022-05-10 07:49:44 -0300

    vista nueva?

--- vision/vista_renderer.gd
+++ vision/vista_renderer.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+export var use_owner_angle := true
+export var angle_degrees := 360
 
 func _draw():
-	var vista = owner
 	var center = Vector2.ZERO
-	var half_vision_angle = vista.angle_degs/2.0
-	RenderUtils.draw_circle_slice(self, center, vista.radius, -half_vision_angle, half_vision_angle, Color.white)
-
+	var vista = owner
+	if use_owner_angle:
+		var half_vision_angle = vista.angle_degs/2.0
+		RenderUtils.draw_circle_slice(self, center, vista.radius, -half_vision_angle, half_vision_angle, Color.white)
+	else:
+		var half_vision_angle = angle_degrees/2.0
+		RenderUtils.draw_circle_slice(self, center, vista.radius, -half_vision_angle, half_vision_angle, Color.white)
+	
